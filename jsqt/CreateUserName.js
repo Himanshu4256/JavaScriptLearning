e20a64c2c21e3af9cdb76dee4378102ccecd1e9b
Himanshu4256 <111674774+Himanshu4256@users.noreply.github.com>
2025-09-19 01:06:08 +0530

    initial commit

---
+++ jsqt/CreateUserName.js
@@ -0,0 +1,3 @@
+let name = prompt("Enter your name");
+let size = name.length;
+console.log("Your name is " + "@"+name+size);
---
+++ jsqt/NumberMatcher.js
@@ -0,0 +1,8 @@
+let num = 25;
+
+let userNum = prompt("Enter a number ");
+
+while(userNum != num){
+userNum = prompt("Enter wrong number please try again");
+}
+console.log("You guessed it right");

dceedba37c804fc71a7fc9f150e05c3343699207
Himanshu4256 <111674774+Himanshu4256@users.noreply.github.com>
2025-12-14 22:29:28 +0530

    initial array commit

---
+++ jsqt/arrays.js
@@ -0,0 +1,11 @@
+let heros = ["one","two","three","four"]
+
+// for loop
+// for(let i = 0; i < heros.length;i++){
+//     console.log(heros[i]);
+// }
+
+// for of loop
+for(let hero of heros){
+    console.log(hero);
+}

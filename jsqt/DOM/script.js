a3a9cb6b053d0b28dd239e0faa40e11b7cd470c1
Himanshu4256 <111674774+Himanshu4256@users.noreply.github.com>
2025-12-20 15:37:00 +0530

    initial commit

--- jsqt/DOM/script.js
+++ jsqt/DOM/script.js
@@ -1,1 +1,6 @@
 console.dir(document.body.childNodes[1])
+
+let newHeading = document.createElement("h1");
+newHeading.innerHTML = "<i>Hi, I m new!</i>";
+
+document.querySelector("body").prepend(newHeading);

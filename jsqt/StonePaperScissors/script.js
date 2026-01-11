1a86a6a6dfea3d3a08cc01084f50ae479673e2c7
Himanshu4256 <111674774+Himanshu4256@users.noreply.github.com>
2026-01-12 01:19:54 +0530

    game cmpleted

--- jsqt/StonePaperScissors/script.js
+++ jsqt/StonePaperScissors/script.js
@@ -5,16 +5,43 @@ choices.forEach(choice => {
     choice.addEventListener("click", () => {
         const userChoice = choice.getAttribute("data-choice");
         const comSelected = genCompChoice();
-        result.innerText = "You selected: " + userChoice + "\n" + "Computer selected: " + comSelected;
 
-        if(userChoice === comSelected){
-            result.innerText += "\nCongratulations!! You Win 🎉";
+        result.innerText =
+            "You selected: " + userChoice + "\n" +
+            "Computer selected: " + comSelected;
+
+        if (userChoice === comSelected) {
+            drawGame(result);
+        } else {
+            let userWin = true;
+
+            if (userChoice === "rock") {
+                userWin = comSelected === "paper" ? false : true;
+            } else if (userChoice === "paper") {
+                userWin = comSelected === "scissors" ? false : true;
+            } else {
+                userWin = comSelected === "rock" ? false : true;
+            }
+
+            showWinner(userWin);
         }
     });
 });
 
 const genCompChoice = () => {
-    const options = ["rock","paper","scissors"];
-    const randIdx = Math.floor(Math.random()*3);
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
     return options[randIdx];
-}
+};
+
+const drawGame = (result) => {
+    result.innerText += "\nDraw game 🤝";
+};
+
+const showWinner = (userWin) => {
+    if (userWin) {
+        result.innerText += "\nYou win! 🎉";
+    } else {
+        result.innerText += "\nYou lose 😢";
+    }
+};

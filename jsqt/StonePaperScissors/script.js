aea85f66cc537a7fa6437c4dd10a449ac75d69ab
Himanshu4256 <111674774+Himanshu4256@users.noreply.github.com>
2026-01-12 01:01:43 +0530

    initial commit

--- jsqt/StonePaperScissors/script.js
+++ jsqt/StonePaperScissors/script.js
@@ -4,6 +4,17 @@ const result = document.getElementById("result");
 choices.forEach(choice => {
     choice.addEventListener("click", () => {
         const userChoice = choice.getAttribute("data-choice");
-        result.innerText = "You selected: " + userChoice;
+        const comSelected = genCompChoice();
+        result.innerText = "You selected: " + userChoice + "\n" + "Computer selected: " + comSelected;
+
+        if(userChoice === comSelected){
+            result.innerText += "\nCongratulations!! You Win 🎉";
+        }
     });
 });
+
+const genCompChoice = () => {
+    const options = ["rock","paper","scissors"];
+    const randIdx = Math.floor(Math.random()*3);
+    return options[randIdx];
+}

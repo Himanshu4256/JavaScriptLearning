738ce85f78a30c4e09c3b4747d2106478569eff5
Himanshu4256 <111674774+Himanshu4256@users.noreply.github.com>
2026-01-18 01:51:42 +0530

    async await learning commit

---
+++ jsqt/asyncAwait.js
@@ -0,0 +1,30 @@
+function getData(dataId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data",dataId);
+            resolve("success");
+        }, 2000);
+    });
+}
+
+
+async function getAllData() {
+    console.log("getting data 1......");
+    await getData(1);
+    console.log("getting data 2......");
+    await getData(2);
+    console.log("getting data 3......");
+    await getData(3);
+}
+
+//getAllData();
+
+// Auto func call example below
+(async function() {
+    console.log("getting data 1......");
+    await getData(1);
+    console.log("getting data 2......");
+    await getData(2);
+    console.log("getting data 3......");
+    await getData(3);
+})();
